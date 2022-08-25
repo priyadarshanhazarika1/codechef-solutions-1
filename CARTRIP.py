ad8fe3bb03c138940de658319f5c332627140340
Sutharsan.S <s.sutharsan.201104@gmail.com>
2022-08-25 11:27:28 +0530

    Add files via upload

---
+++ CARTRIP.py
@@ -0,0 +1,10 @@
+# cook your dish here
+T=int(input())
+for i in range(0,T):
+    X=int(input())
+    if X>300:
+        print(X*10)
+    elif X<=300:
+        print(300*10)
+
+        # TODO: write code...
