9631853ad1a3d31f26f7932d3a8b2a541e6e123f
Sutharsan.S <s.sutharsan.201104@gmail.com>
2022-09-02 21:01:15 +0530

    Add files via upload

---
+++ CABS.py
@@ -0,0 +1,12 @@
+# cook your dish here
+T=int(input())
+for i in range(0,T):
+    X,Y=input().split()
+    x=int(X)
+    y=int(Y)
+    if(x<y):
+        print("FIRST")
+    elif(x==y):
+        print("ANY")
+    else:
+        print("SECOND")
