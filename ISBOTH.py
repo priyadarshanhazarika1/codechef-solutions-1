b557e75495107de04e4e6f717a10c9f45b23c4fe
Sutharsan.S <s.sutharsan.201104@gmail.com>
2022-09-07 19:03:45 +0530

    Add files via upload

---
+++ ISBOTH.py
@@ -0,0 +1,11 @@
+# cook your dish here
+try:
+    n=int(input())
+    if(n%5==0 and n%11==0):
+        print("BOTH")
+    elif(n%5==0 or n%11==0):
+        print("ONE")
+    else:
+        print("NONE")
+except:
+    pass
